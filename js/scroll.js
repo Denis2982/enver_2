5085e24f73a92249a5ee74d074ee745f8189a1eb
Denis2982 <denisdenis2982@gmail.com>
2023-09-13 18:58:37 +0300

    change scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -2,11 +2,9 @@ const anchors = document.querySelectorAll('a[href*="#"]')
 
 for (let anchor of anchors) {
   anchor.addEventListener('click', function (e) {
-    e.preventDefault()
-    
-    const blockID = anchor.getAttribute('href').substr(1)
-    
-    document.getElementById(blockID).scrollIntoView({
+    e.preventDefault();    
+    const blockID = anchor.getAttribute('href')    
+    document.querySelector('' + blockID).scrollIntoView({
       behavior: 'smooth',
       block: 'start'
     })
